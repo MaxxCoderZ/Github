401902102e6447b148c0566a441f7d4fd4293c11
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-06-01 20:49:33 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,18 +1,21 @@
 const jsonFile = require('jsonfile);
 const moment = require('moment');
 const simpleGit = require('simple-git');
-
+const random = require ('random');
 const FILE_PATH = './data.json';
-
-const makeCommit = (x,y) => {
-Const DATE = moment().substartct(1,'y').add(1, 'd')
+const makeCommit = n => {
+ if(n===0) return simpleGit().push();
+ const x = random.int(0,54);
+ const y = random.int(0,6);
+ Const DATE = moment().substartct(1,'y').add(1, 'd')
               .add(x,  'w').add(y,  'd'.format();
 const data = {
   date : DATE
 }
-
+console.log(DATE);
 jsonfile.writefile(FILE_PATH, data, ()=>{
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }).push();
+ simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, 
+ makecommit.bind(this, --n));
 });
 
-makecommit(3,3)
+makecommit(100);
